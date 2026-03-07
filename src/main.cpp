eb7f6eaf724800fa4f983fd62b9dd4e48d44ad87
Yatsko Maksym <lorewealth@gmail.com>
2026-03-07 22:38:36 +0000

    Am testat functionarea Makefile si daca merge corect rularea programului

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+#include "header.h"
 
 int main(){
 
     std::cout << "Hello, World!\n";
+    Test t1(30, "He");
+    std::cout << t1.getName() << ' ' << t1.getUnits() << '\n';
 
     return 0;
 }
